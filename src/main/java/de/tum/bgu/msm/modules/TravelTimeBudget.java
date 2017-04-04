9c61494fc5b0f3ad9c58d230250660ae2d00b18b
Rolf Moeckel <rolf.moeckel@udo.edu>
2017-04-04 20:46:49 +0200

    Using different UtilityExpressionCalculator. Just a test for SILO.

--- src/main/java/de/tum/bgu/msm/modules/TravelTimeBudget.java
+++ src/main/java/de/tum/bgu/msm/modules/TravelTimeBudget.java
@@ -49,13 +49,13 @@ public class TravelTimeBudget {
 
         logger.info("  Creating Utility Expression Calculator for microscopic travel time budget calculation.");
         logCalculation  = ResourceUtil.getBooleanProperty(rb, PROPERTIES_LOG_UTILITY_CALCULATION_TOTAL_TTB);
-        String uecFileName     = ResourceUtil.getProperty(rb, PROPERTIES_TRAVEL_TIME_BUDGET_UEC_FILE);
+        String uecFileName     = rb.getString(PROPERTIES_TRAVEL_TIME_BUDGET_UEC_FILE);
         int totalTtbSheetNumber = ResourceUtil.getIntegerProperty(rb, PROPERTIES_Total_Travel_Time_Budget_UEC_UTILITY);
         int dataSheetNumber = ResourceUtil.getIntegerProperty(rb, PROPERTIES_TRAVEL_TIME_BUDGET_UEC_DATA_SHEET);
         totalTtbUtility = new UtilityExpressionCalculator(new File(uecFileName),
                 totalTtbSheetNumber,
                 dataSheetNumber,
-                rb,
+                ResourceUtil.changeResourceBundleIntoHashMap(rb),
                 TravelTimeBudgetDMU.class);
         totalTravelTimeBudgetDMU = new TravelTimeBudgetDMU();
 
