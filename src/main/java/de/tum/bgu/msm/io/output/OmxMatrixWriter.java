0262ee8bba4ae4a204a4cc5b768d66a2bbc95f7d
nkuehnel <nkuehnel2@gmail.com>
2019-03-25 12:42:06 +0100

    omx lookup is not created on file creation but on matrix creation only

--- src/main/java/de/tum/bgu/msm/io/output/OmxMatrixWriter.java
+++ src/main/java/de/tum/bgu/msm/io/output/OmxMatrixWriter.java
@@ -15,19 +15,10 @@ public class OmxMatrixWriter {
     public static void createOmxFile(String omxFilePath, int numberOfZones) {
 
         try (OmxFile omxFile = new OmxFile(omxFilePath)) {
-            int dim0 = numberOfZones -1;
+            int dim0 = numberOfZones;
             int dim1 = dim0;
             int[] shape = {dim0, dim1};
-            int lookup1NA = -1;
-            int[] lookup1Data = new int[dim0];
-            Set<Integer> lookup1Used = new HashSet<>();
-            for (int i = 0; i < lookup1Data.length; i++) {
-                int lookup = i + 1;
-                lookup1Data[i] = lookup1Used.add(lookup) ? lookup : lookup1NA;
-            }
-            OmxLookup.OmxIntLookup lookup1 = new OmxLookup.OmxIntLookup("lookup", lookup1Data, lookup1NA);
             omxFile.openNew(shape);
-            omxFile.addLookup(lookup1);
             omxFile.save();
 
         }
