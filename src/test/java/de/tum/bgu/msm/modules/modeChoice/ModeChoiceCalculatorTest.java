82a856421549648feee5c91bb33fd4a8f287ee4c
Nico <nkuehnel2@gmail.com>
2019-07-08 13:44:20 +0200

    implement travel time _FROM_ region in addition to travel time to region (used in SILO housing search)

--- src/test/java/de/tum/bgu/msm/modules/modeChoice/ModeChoiceCalculatorTest.java
+++ src/test/java/de/tum/bgu/msm/modules/modeChoice/ModeChoiceCalculatorTest.java
@@ -50,10 +50,16 @@ public class ModeChoiceCalculatorTest {
         		}
         	}
 
-			@Override
-			public double getTravelTimeToRegion(Location origin, Region destination, double timeOfDay_s, String mode) {
-				return 0;
-			}
+            @Override
+            public double getTravelTimeFromRegion(Region origin, Zone destination, double timeOfDay_s, String mode) {
+                return 0;
+            }
+
+            @Override
+            public double getTravelTimeToRegion(Zone origin, Region destination, double timeOfDay_s, String mode) {
+                return 0;
+            }
+
 
             @Override
             public IndexedDoubleMatrix2D getPeakSkim(String mode) {
