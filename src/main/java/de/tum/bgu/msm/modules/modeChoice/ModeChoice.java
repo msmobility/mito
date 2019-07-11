c4ad52f1f60dd8a231c30a940a2925b98334ab15
ga84tey <ga84tey@mytum.de>
2019-07-11 16:34:27 +0200

    bug script

--- src/main/java/de/tum/bgu/msm/modules/modeChoice/ModeChoice.java
+++ src/main/java/de/tum/bgu/msm/modules/modeChoice/ModeChoice.java
@@ -153,7 +153,7 @@ public class ModeChoice extends Module {
                         .getResourceAsStream("ModeChoiceAV")), purpose);
             } else if (includeUAM) {
                 this.calculator = new ModeChoiceJSCalculator(new InputStreamReader(this.getClass()
-                        .getResourceAsStream("ModeChoiceUAM")), purpose);
+                        .getResourceAsStream("ModeChoiceUAMIncremental")), purpose);
             } else{
                 this.calculator = new ModeChoiceJSCalculator(new InputStreamReader(this.getClass()
                         .getResourceAsStream("ModeChoice")), purpose);
