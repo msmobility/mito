b2df485ebcf4ffcfafb774daebdcba40ae7335a5
cllorca1 <carlos.llorca@tum.de>
2019-08-27 15:44:01 +0200

    bug in searching time interval and not found zone id

--- src/main/java/de/tum/bgu/msm/data/waitingTimes/StationDependentTotalHandlingTimes.java
+++ src/main/java/de/tum/bgu/msm/data/waitingTimes/StationDependentTotalHandlingTimes.java
@@ -5,6 +5,7 @@ import de.tum.bgu.msm.data.MitoTrip;
 import de.tum.bgu.msm.data.Mode;
 import de.tum.bgu.msm.data.accessTimes.AccessAndEgressVariables;
 
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -29,14 +30,14 @@ public class StationDependentTotalHandlingTimes implements TotalHandlingTimes {
         if (mode.equalsIgnoreCase(Mode.uam.toString())) {
             double timeOfDay_s2 = trip.getDepartureInMinutes() * 60;
             int zoneId = (int) accessAndEgressVariables.getAccessVariable(origin, destination, mode, AccessAndEgressVariables.AccessVariable.ACCESS_VERTIPORT);
-            if (zoneId != -1) {
+            if (zoneId != 10000) {
                 String station = zonesToStationMap.get(zoneId);
-                for (int interval : averageWaitingTimesByUAMStationAndTime_min.get(station).keySet()) {
-                    if (timeOfDay_s2 > interval) {
-                        return averageWaitingTimesByUAMStationAndTime_min.get(station).get(interval);
-                    }
+                Iterator<Integer> intervals = averageWaitingTimesByUAMStationAndTime_min.get(station).keySet().iterator();
+                int interval = intervals.next();
+                while (intervals.hasNext() && timeOfDay_s2 > interval){
+                        interval = intervals.next();
                 }
-                return averageWaitingTimesByUAMStationAndTime_min.get(station).get(averageWaitingTimesByUAMStationAndTime_min.get(station).size());
+                return averageWaitingTimesByUAMStationAndTime_min.get(station).get(interval);
             } else {
                 //did not have an actual UAM-plausible-route but choose still UAM - this is not consistent between mito and uam-matsim.
                 //in MITO, the trip that is faster by car is assigned with travel time = 10000 min, so it should not be done (although
