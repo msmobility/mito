dd637c6ca845995b013f985288dc47356bc6797d
cllorca1 <carlos.llorca@tum.de>
2019-09-06 08:50:43 +0200

    run as many simulations as property files in args

--- src/main/java/de/tum/bgu/msm/MitoMuc.java
+++ src/main/java/de/tum/bgu/msm/MitoMuc.java
@@ -5,17 +5,29 @@ import org.apache.log4j.Logger;
 
 /**
  * Implements the Transport in Microsimulation Orchestrator (TIMO)
+ *
  * @author Rolf Moeckel
  * Created on Feb 12, 2017 in Munich, Germany
- *
  */
 class MitoMuc {
 
     private static final Logger logger = Logger.getLogger(MitoMuc.class);
 
     public static void main(String[] args) {
-        logger.info("Started the Microsimulation Transport Orchestrator (MITO)");
-        MitoModel model = MitoModel.standAloneModel(args[0], MunichImplementationConfig.get());
-        model.runModel();
+
+        for (String arg : args) {
+
+            try {
+                logger.info("Started the Microsimulation Transport Orchestrator (MITO)");
+                logger.info(arg);
+                MitoModel model = MitoModel.standAloneModel(arg, MunichImplementationConfig.get());
+                model.runModel();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 }
