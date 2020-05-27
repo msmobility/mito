f9f0aec884c8c7141bd3544290711fc0c248d76e
Nico <nkuehnel2@gmail.com>
2020-05-27 03:37:45 +0200

    update tests

--- src/test/java/de/tum/bgu/msm/modules/travelTimeBudget/TravelTimeBudgetCalculatorTest.java
+++ src/test/java/de/tum/bgu/msm/modules/travelTimeBudget/TravelTimeBudgetCalculatorTest.java
@@ -6,9 +6,6 @@ import de.tum.bgu.msm.resources.Resources;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.InputStreamReader;
-import java.io.Reader;
-
 import static org.junit.Assert.assertEquals;
 
 public class TravelTimeBudgetCalculatorTest {
@@ -22,7 +19,6 @@ public class TravelTimeBudgetCalculatorTest {
 
         Resources.initializeResources("./test/muc/test.properties");
 
-        Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("TravelTimeBudgetCalc"));
         calculator = new TravelTimeBudgetCalculatorImpl();
 
         dataSet = new DataSet();
